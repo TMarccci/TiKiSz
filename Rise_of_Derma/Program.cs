e777ad920fa7911832198ecf4fa68d6403ca8ce9
Tihanyi Marcell <marcitihanyi@gmail.com>
2024-03-18 20:55:09 +0100

    Scenario and Main

--- Rise_of_Derma/Program.cs
+++ Rise_of_Derma/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Import Libraries
+using Rise_of_Derma.scenarios;
+
+// Initial Windows Setup (Pragma for ignore alert)
+#pragma warning disable CA1416
+Console.SetWindowSize(82, 24);
+Console.Title = "Rise of Derma";
+#pragma warning restore CA1416
+
+// Demo
+Scenario currentScenario = new DemoScenario();
+
+// Main game loop + Controls
+ConsoleKeyInfo cki = new ConsoleKeyInfo();
+
+do
+{
+    while (Console.KeyAvailable == false)
+        Thread.Sleep(250);
+    cki = Console.ReadKey(true);
+
+    currentScenario.ProcessInput(cki);
+
+} while (cki.Key != ConsoleKey.X);
