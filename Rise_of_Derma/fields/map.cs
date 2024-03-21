f4478855dcebfe34948853842b62149767230049
Pesek-Patrik <105454371+Pesek-Patrik@users.noreply.github.com>
2024-03-21 16:52:29 +0100

    Add files via upload

---
+++ Rise_of_Derma/fields/map.cs
@@ -0,0 +1,11 @@
+﻿namespace Rise_of_Derma.fields
+{
+    class Map
+    {
+        private int h = 20;
+        private int w = 80;
+
+        public int H { get { return h; } }
+        public int W { get { return w; } }
+    }
+}
