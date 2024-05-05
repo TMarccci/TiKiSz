eed17ab06d7141488ef786db1c82f16fb2121f17
Szabó Dávid <szabodavid202@gmail.com>
2024-05-05 19:20:01 +0200

    skeleton class letrehozasa

---
+++ Rise_of_Derma/entities/skeleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rise_of_Derma.entities
+{
+    internal class skeleton
+    {
+    }
+}
