b09b370a84e6663c88940e548a5f20628147bc52
Kiss Dominik <kissdominikbence@gmail.com>
2024-05-29 17:51:44 +0200

    maptool debug

--- Rise_of_Derma_Maptool/maptool.py
+++ Rise_of_Derma_Maptool/maptool.py
@@ -5,8 +5,8 @@ wb = openpyxl.load_workbook(file, read_only=True)
 ws = wb.active
 
 with open("./Finalized.txt", "w", encoding="utf-8") as f:
-    for i in range(1, ws.max_row+1):
-        for j in range(1, ws.max_column+1):
+    for i in range(2, ws.max_row+1):
+        for j in range(2, ws.max_column+1):
             if ws.cell(row=i, column=j).value is not None:
                 cellvalue = ws.cell(row=i, column=j).value
                 numbers = [str(x+1) for x in range(9)]
@@ -19,7 +19,7 @@ with open("./Finalized.txt", "w", encoding="utf-8") as f:
                     f.write(f"{j-2};{i-2};FINISHLEVEL\n")
                 elif cellvalue == "K":
                     f.write(f"{j-2};{i-2};CRYSTAL\n")
-                elif cellvalue in numbers:
+                elif str(cellvalue) in numbers:
                     f.write(f"{j-2};{i-2};ENEMY;{cellvalue}\n")
     
     f.close()
